bb46b96e25c6b39c2d8c1d536e423184b9629abe
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-07-19 09:06:00 -0500

    testing-web

---
+++ src/test/java/edu/escuelaing/arsw/app/TestingWebApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.escuelaing.arsw.app;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
