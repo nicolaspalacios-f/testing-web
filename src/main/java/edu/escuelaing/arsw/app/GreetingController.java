9b2a04acd84b9744c1476eeaa1dc9365f9290544
nicolaspalacios-f <nicolas.palacios-f@mail.escuelaing.edu.co>
2022-07-19 09:32:46 -0500

    finalizacion quiz

---
+++ src/main/java/edu/escuelaing/arsw/app/GreetingController.java
@@ -0,0 +1,21 @@
+package edu.escuelaing.arsw.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+    private final GreetingService service;
+
+    public GreetingController(GreetingService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/greeting")
+    public @ResponseBody String greeting() {
+        return service.greet();
+    }
+
+}
